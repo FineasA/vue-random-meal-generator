66d74c30c6e0c2f8297a1d1841246670c8e4580a
Fineas Anton <fineas.anton@gmail.com>
2020-03-06 14:35:01 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container animated fadeIn">
+    <div class="row">
+      <h1 class="title">Random Meal Generator</h1>
+    </div>
+    <hr />
+    <div class="row">
+      <button class="btn btn-primary" @click="getMeal">
+        Get Meal 🍔
+      </button>
+    </div>
+    <br />
+    <transition
+      enter-active-class="animated fadeIn"
+      leave-active-class="animated fadeOut"
+    >
+      <meal-display v-if="mealActive" :meal="meal"></meal-display>
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MealDisplay from "./components/MealDisplay.vue";
 
 export default {
-  name: "App",
   components: {
-    HelloWorld
+    MealDisplay
+  },
+  data() {
+    return {
+      mealActive: false,
+      meal: {
+        name: "",
+        instructions: "",
+        youtubeLink: "",
+        ingredients: [],
+        measurements: [],
+        ingredientMeasure: [],
+        mealImg: ""
+      }
+    };
+  },
+  methods: {
+    clearData() {
+      this.meal = {
+        name: "",
+        instructions: "",
+        youtubeLink: "",
+        ingredients: [],
+        measurements: [],
+        ingredientMeasure: [],
+        mealImg: ""
+      };
+    },
+    getMeal() {
+      if (this.mealActive) {
+        this.mealActive = false;
+        this.clearData();
+        return;
+      }
+      this.mealActive = true;
+      this.$http
+        .get("https://www.themealdb.com/api/json/v1/1/random.php")
+        .then(response => {
+          console.log(response);
+          this.meal.name = response.data.meals[0].strMeal;
+          this.meal.instructions = response.data.meals[0].strInstructions;
+          this.meal.youtubeLink = response.data.meals[0].strYoutube;
+          this.meal.mealImg = response.data.meals[0].strMealThumb;
+          //get ingredients and measurements
+          const keys = Object.entries(response.data.meals[0]);
+          for (let i = 0; i < keys.length; i++) {
+            if (keys[i][0].includes("strIngredient") && keys[i][1].length > 0) {
+              this.meal.ingredients.push(keys[i][1]);
+            } else if (
+              keys[i][0].includes("strMeasure") &&
+              keys[i][1].length > 0
+            ) {
+              this.meal.measurements.push(keys[i][1]);
+            }
+          }
+          //neatly put name of ingredient and measurement into array
+          for (let i = 0; i < this.meal.ingredients.length; i++) {
+            this.meal.ingredientMeasure.push([
+              this.meal.ingredients[i],
+              this.meal.measurements[i]
+            ]);
+          }
+        });
+    }
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.title,
+.btn,
+.card {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/MealDisplay.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card" style="width: 50%; text-align: justify">
+    <img :src="meal.mealImg" class="card-img-top" :alt="meal.name" />
+
+    <div class="card-body">
+      <h4 class="card-text" style="text-align: center">
+        {{ meal.name }}
+      </h4>
+      <a
+        style="display: block; text-align:center; margin: 0 auto;"
+        class="card-link"
+        :href="meal.youtubeLink"
+        >Watch Video!</a
+      >
+      <hr />
+      <h3 style="text-align:center" class="card-text">Instructions</h3>
+      <p class="card-text">{{ meal.instructions }}</p>
+      <hr />
+      <h3 style="text-align: center" class="card-text">Ingredients</h3>
+      <ul class="list-group list-group-flush">
+        <li
+          v-for="(ingredient, index) in meal.ingredientMeasure"
+          :key="index"
+          class="list-group-item"
+        >
+          {{ ingredient[1] }} {{ ingredient[0] }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    meal: Object
+  }
+};
+</script>
+
+<style></style>
